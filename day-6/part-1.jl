ba8fbd997905826cf4d2772131828ebf486ea696
Merry <git@mary.rs>
2021-12-08 21:48:26 +0000

    day 6

---
+++ day-6/part-1.jl
@@ -0,0 +1,20 @@
+function main()
+    input = read("input", String)
+    input = split.(strip(input), ",")
+    input = parse.(Int64, input)
+    input .+= 1
+
+    state = [0,0,0,0,0,0,0,0,0]
+
+    state = sum(input' .== 1:9, dims=2)[:]
+
+    for day = 1:80
+        births = first(state)
+        state = circshift(state, -1)
+        state[7] += births
+    end
+
+    display(sum(state))
+end
+
+main()
