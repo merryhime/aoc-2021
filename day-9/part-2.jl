e8123efb0d58f05cafa2aaeb018047f90137c3be
Merry <git@mary.rs>
2021-12-09 16:18:53 +0000

    simplify day 9 part 2

--- day-9/part-2.jl
+++ day-9/part-2.jl
@@ -1,7 +1,7 @@
 using Underscores
 
 function main()
-    original = @_ readlines("input") |> map(parse.(Int, collect(_)), __) |> vcat(__'...)
+    field = @_ readlines("input") |> map(parse.(Int, collect(_)), __) |> vcat(__'...)
 
     rotu(m, f) = [ m[2:end,:]            ; fill(f, 1, size(m,2)) ]
     rotd(m, f) = [ fill(f, 1, size(m,2)) ; m[1:end-1,:]          ]
@@ -11,36 +11,26 @@ function main()
     mins(m) = m .< rotu(m, 9) .&& m .< rotd(m, 9) .&& m .< rotl(m, 9) .&& m .< rotr(m, 9)
     prop(m) = m .|| rotu(m, false) .|| rotd(m, false) .|| rotl(m, false) .|| rotr(m, false)
 
-    field = deepcopy(original)
     basins = mins(field)
     depth = field .!= 9
 
     basin_sizes = zeros(0)
-
     for i in eachindex(basins)
-        if !basins[i]
-            continue
-        end
+        basins[i] || continue
 
+        prev = falses(size(basins))
         basin = falses(size(basins))
         basin[i] = true
 
-        while true
-            p = prop(basin) .&& depth
-
-            if sum(p .&& .!basin) == 0
-                break
-            end
-
-            basin[p] .= true
+        while basin != prev
+            prev = basin
+            basin = prop(prev) .&& depth
         end
 
-        sz = sum(basin)
-
-        append!(basin_sizes, sz)
+        append!(basin_sizes, count(basin))
     end
 
-    @_ basin_sizes |> sort(__, rev=true) |> Iterators.take(__, 3) |> prod(__) |> display(__)
+    @_ basin_sizes |> sort(__, rev=true) |> Iterators.take(__, 3) |> prod |> display
 end
 
 main()
