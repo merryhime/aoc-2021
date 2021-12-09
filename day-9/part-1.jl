0eec0508220390d7e8409a9c63d2f1437ff019d8
Merry <git@mary.rs>
2021-12-09 13:24:14 +0000

    day 9: part 1

---
+++ day-9/part-1.jl
@@ -0,0 +1,17 @@
+using Underscores
+using ImageFiltering
+
+function main()
+    field = @_ readlines("input") |> map(parse.(Int, collect(_)), __) |> vcat(__'...)
+
+    rotu = [ field[2:end,:]             ; fill(10, 1, size(field,2)) ]
+    rotd = [ fill(10, 1, size(field,2)) ; field[1:end-1,:]           ]
+    rotl = [ field[:,2:end]               fill(10, size(field,1), 1) ]
+    rotr = [ fill(10, size(field,1), 1)   field[:,1:end-1]           ]
+
+    mins = field .< rotu .&& field .< rotd .&& field .< rotl .&& field .< rotr
+
+    display(sum(field[mins] .+ 1))
+end
+
+main()
