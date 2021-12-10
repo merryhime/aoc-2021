727c825bdd19ca05369b49c97d9fb855286e0cef
Merry <git@mary.rs>
2021-12-10 10:36:41 +0000

    use missing

--- day-10/part-2.jl
+++ day-10/part-2.jl
@@ -9,12 +9,12 @@ function get_completion(seq)
     s = []
     for c in collect(seq)
         c ∈ opening && push!(s, c)
-        c ∈ closing && c != get(matching, pop!(s), nothing) && return nothing
+        c ∈ closing && c != get(matching, pop!(s), nothing) && return missing
     end
     return reverse(s)
 end
 
 score_completion(s) = @_ reduce(_1 * 5 + get(points, _2, nothing), s; init=0)
 
-scores = @_ readlines("input") |> map(get_completion, __) |> filter(_ != nothing, __) |> map(score_completion, __)
+scores = @_ readlines("input") |> map(get_completion, __) |> skipmissing |> map(score_completion, __)
 display(sort(scores)[length(scores)÷2 + 1])
