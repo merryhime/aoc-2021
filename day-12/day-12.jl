4abe7df7064e1f202e9749a098be91c0a99bc690
Merry <git@mary.rs>
2021-12-12 11:16:36 +0000

    day 12: simplify

--- day-12/day-12.jl
+++ day-12/day-12.jl
@@ -1,32 +1,24 @@
 using Query, Underscores
 
 input = @_ readlines("input") |> split.(__, "-")
-input = [input; (input |> @map([_[2], _[1]]) |> collect)]
+input = [input; (input |> @map(reverse(_)) |> collect)]
 cavemap = input |> @filter(_[1] != "end" && _[2] != "start") |> @groupby(_[1], _[2]) |> @map((key(_), collect(_))) |> Dict
 
 issmall(x) = islowercase(x[1])
-candup(x, path) = !issmall(x) || x ∉ path
+isdup(x, path) = issmall(x) && x ∈ path
 
-function walk1(path = ["start"])
+function walk(path = ["start"], candup=false)
     if last(path) == "end"
         return 1
     end
     nexts = get(cavemap, last(path), nothing)
-    return @_ nexts |> @filter(candup(_, path)) |> @map(walk1([path; _])) |> reduce(+, __; init=0)
+    return @_ nexts |> @filter(candup || !isdup(_, path)) |> @map(walk([path; _], candup && !isdup(_, path))) |> reduce(+, __; init=0)
 end
 
 function part1()
-    display(walk1())
-end
-
-function walk2(path = ["start"], hasdup=false)
-    if last(path) == "end"
-        return 1
-    end
-    nexts = get(cavemap, last(path), nothing)
-    return @_ nexts |> @filter(!hasdup || candup(_, path)) |> @map(walk2([path; _], hasdup || !candup(_, path))) |> reduce(+, __; init=0)
+    display(walk(["start"], false))
 end
 
 function part2()
-    display(walk2())
+    display(walk(["start"], true))
 end
