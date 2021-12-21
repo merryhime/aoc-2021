60d4ac330c104f317ebdbf339114e68fc9bc39f0
Merry <git@mary.rs>
2021-12-21 20:44:42 +0000

    day 21: minor performance

--- day-21/part-2.jl
+++ day-21/part-2.jl
@@ -17,20 +17,21 @@ const rollsums = counter([i+j+k for i ∈ 1:3 for j ∈ 1:3 for k ∈ 1:3])
 function gen(p::Player)
     ps = counter([p]);
     wins = Accumulator{Player, Int64}()
-    lose = Dict{Int, Accumulator{Player, Int64}}()
+    lose = Dict{Int, Vector{Pair{Player, Int64}}}()
     turns = 1
     while !isempty(ps)
         ps = reducecount(turn(p.first, r.first) => p.second * r.second for p ∈ ps for r ∈ rollsums)
         wins = merge(wins, Accumulator(Dict(filter(iswin, ps))))
-        ps = lose[turns] = Accumulator(Dict(filter(!iswin, ps)))
+        ps = lose[turns] = collect(filter(!iswin, ps))
         turns += 1
     end
     return wins, lose
 end
 
-a_win, a_lose = gen(Player(0, 4, 0))
-b_win, b_lose = gen(Player(0, 2, 0))
+input = readlines("input") |> @map(parse(Int, match(r"Player \d starting position: (\d+)", _)[1])) |> collect
+
+a_win, a_lose = gen(Player(0, input[1], 0))
+b_win, b_lose = gen(Player(0, input[2], 0))
 
 display(sum([w.second * l.second for w ∈ a_win for l ∈ b_lose[w.first.turns - 1]]))
 display(sum([w.second * l.second for w ∈ b_win for l ∈ a_lose[w.first.turns]]))
-
